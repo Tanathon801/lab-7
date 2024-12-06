40fe00212336b3daa18418232dda4592c45acd1d
Tanathon801 <aumatomic@gmail.com>
2024-12-06 15:42:28 +0700

    Update lab7_2.cpp

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -16,3 +16,35 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+#include <iostream>
+#include <string> 
+using namespace std;
+
+int main() {
+   string x; 
+   int y;
+   string z; 
+   string c; 
+   string t; 
+cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+cout << "?????: ";
+getline(cin,x);
+cout << "Fahsai: Wow!!! "<< x << " is a really cool name.\n";
+cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+cout <<  x << ": ";
+cin >> y;
+cout << "Fahsai: I think you may be GEAR " <<y/10000000-12<<". I have a free movie ticket for you.\n";
+cout <<"Fahsai: Let's go to the cinema together!!!\n";
+cout << "Fahsai: What movie do you want to watch?\n";
+cin.ignore();
+cout <<  x << ": ";
+getline(cin,z);
+cout << "Fahsai: So....which day are you free to go with me?\n";
+cout <<  x << ": ";
+getline(cin,c);
+cout << "Fahsai: " << c << "....that is OK!!! I'm looking forward to watching " << z << " with you.\n";
+cout <<  x << ": ";
+getline(cin,t);
+cout << "Fahsai: 555+ see you " << c << ". Bye Bye \\(^ ^)/\n";
+    return 0;
+}
